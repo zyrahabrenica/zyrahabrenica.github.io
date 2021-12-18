147ed7ec751cfe0ac8b79386d5edd804e4f32e33
zyrahabrenica <zeeabrenica@gmail.com>
2021-12-18 19:45:36 +0800

    First Commit

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "zeeabrenica@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+?>
